baffccbe2710a74d1b87613c88f74c3c9b6392da
ryanfawcett <tangch3ng@gmail.com>
2021-11-29 10:06:39 +0800

    feat: 添加订单实体类

---
+++ order-service/src/main/java/com/example/order/pojo/Order.java
@@ -0,0 +1,15 @@
+package com.example.order.pojo;
+
+import lombok.Data;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@Alias("Order")
+public class Order {
+    private Long id;
+    private String name;
+    private String price;
+    private Integer num;
+    private Long userId;
+    private User user;
+}
