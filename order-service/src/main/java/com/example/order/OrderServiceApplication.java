f8441e4ca3482943c2f145f716bdefbd016df935
ryanfawcett <tangch3ng@gmail.com>
2021-11-29 10:05:54 +0800

    feat: 映射Mapper文件并注入RestTemplate

--- order-service/src/main/java/com/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -1,13 +1,22 @@
 package com.example.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@MapperScan("com.example.order.mapper")
 @SpringBootApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
