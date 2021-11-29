1408fed801226877966e4925576015cba8d41552
ryanfawcett <tangch3ng@gmail.com>
2021-11-29 15:04:13 +0800

    feat: 配置负载均衡

--- order-service/src/main/java/org/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/org/example/order/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package org.example.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class OrderServiceApplication {
     }
 
     @Bean
+    @LoadBalanced // 负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
