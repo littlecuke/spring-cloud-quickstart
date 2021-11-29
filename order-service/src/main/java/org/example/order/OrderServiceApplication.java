f367caddbf6190da3e22024cb8606b4b25324dba
ryanfawcett <tangch3ng@gmail.com>
2021-11-29 14:56:21 +0800

    feat: 开启服务发现

--- order-service/src/main/java/org/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/org/example/order/OrderServiceApplication.java
@@ -3,10 +3,12 @@ package org.example.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@MapperScan("com.example.order.mapper")
+@EnableEurekaClient
+@MapperScan("org.example.order.mapper")
 @SpringBootApplication
 public class OrderServiceApplication {
 
