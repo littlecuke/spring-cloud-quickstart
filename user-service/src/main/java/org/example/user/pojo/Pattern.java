106ef6ab22c0649b11e821fc082c372613cf3f48
ryanfawcett <tangch3ng@gmail.com>
2021-12-07 17:16:30 +0800

    feat: 配置Nacos配置文件热更新

---
+++ user-service/src/main/java/org/example/user/pojo/Pattern.java
@@ -0,0 +1,12 @@
+package org.example.user.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "pattern") // 配置热更新
+public class Pattern {
+    private String dateformat;
+}
--- user-service/src/main/java/org/example/user/web/ConfigController.java
+++ user-service/src/main/java/org/example/user/web/ConfigController.java
@@ -1,23 +1,41 @@
 package org.example.user.web;
 
+import org.example.user.pojo.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/config")
+@RefreshScope // 配置热更新
 public class ConfigController {
 
     @Value("${pattern.dateformat}")
     private String dateformat;
+    @Autowired
+    private Pattern pattern;
 
     /**
      * 获取Nacos服务中管理的配置
+     *
+     * 方式一：@RefreshScope
      */
     @GetMapping
     public String remoteConfig() {
         return dateformat;
+    }
+
+    /**
+     * 配置热更新
+     *
+     * 方式二：@ConfigurationProperties
+     */
+    @GetMapping("/properties")
+    public Pattern configProperties() {
+        return pattern;
     }
 
 }
