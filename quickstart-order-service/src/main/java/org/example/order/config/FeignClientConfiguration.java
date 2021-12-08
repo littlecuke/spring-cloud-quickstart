f64d0dcb40adc18e5c7998d3a15ea27ec8a393c3
ryanfawcett <tangch3ng@gmail.com>
2021-12-08 14:05:29 +0800

    docs: Feign底层实现及性能优化

--- quickstart-order-service/src/main/java/org/example/order/config/FeignClientConfiguration.java
+++ quickstart-order-service/src/main/java/org/example/order/config/FeignClientConfiguration.java
@@ -9,6 +9,15 @@ import org.springframework.context.annotation.Bean;
  * 使用方式:
  * - 全局配置：@EnableFeignClients(defaultConfiguration = FeignClientConfiguration.class)
  * - 局部配置：@FeignClient(value = "userservice", configuration = FeignClientConfiguration.class)
+ *
+ * 底层客户端实现:
+ *  - URLConnection: 默认实现不支持连接池
+ *  - Apache HttpClient: 支持连接池
+ *  - OkHttp: 支持连接池
+ *
+ * Feign性能优化:
+ *  - 使用连接池代替默认的URLConnection
+ *  - 日志级别，最好使用 BASIC 或 NONE
  */
 public class FeignClientConfiguration {
     @Bean
