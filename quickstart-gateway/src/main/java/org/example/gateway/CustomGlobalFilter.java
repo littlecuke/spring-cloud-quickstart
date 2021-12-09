b93f5ffb2bbfc3daaccc1c96ab508b3806aa1af7
ryanfawcett <tangch3ng@gmail.com>
2021-12-09 11:44:52 +0800

    docs: 过滤器的执行顺序

--- quickstart-gateway/src/main/java/org/example/gateway/CustomGlobalFilter.java
+++ quickstart-gateway/src/main/java/org/example/gateway/CustomGlobalFilter.java
@@ -8,6 +8,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+/**
+ * 请求进入网关后会经过三类过滤器：当前路由配置的过滤器、DefaultFilter、GlobalFilter，
+ * 请求路由后会将当前路由过滤器、DefaultFilter和GlobalFilter合并到一个过滤器链(集合)中，
+ * 排序后依次执行每个过滤器
+ *
+ * 执行顺序：
+ * 1.每个过滤器都必须要指定一个int类型的order值，order值越小，优先级越高，执行顺序越靠前
+ * 2.GlobalFilter通过实现Ordered接口或者使用@Order注解自定义order值
+ * 3.路由过滤器和DefaultFilter的order值由Spring决定，默认是按照申明的顺序从1递增
+ * 4.当过滤器的order值一样时，执行顺序为：DefaultFilter > 当前路由配置的过滤器 > GlobalFilter
+ */
 // @Order(-1)
 @Slf4j
 @Component
