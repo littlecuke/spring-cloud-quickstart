10c9b6fc3ba7ece903e7b2591d159093b25c961e
ryanfawcett <tangch3ng@gmail.com>
2021-12-17 10:09:57 +0800

    fix: 解决Consumer服务启动报错

--- quickstart-rabbitmq/quickstart-mq-consumer/src/main/java/org/example/mq/listener/SimpleQueueListener.java
+++ quickstart-rabbitmq/quickstart-mq-consumer/src/main/java/org/example/mq/listener/SimpleQueueListener.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * 注意：这里的simple.queue如果是第一次启动会报错，找不到该队列，
+ * 需要Publisher服务中先将消息发送到simple.queue的队列中
+ */
 @Slf4j
-// @Component
+@Component
 public class SimpleQueueListener {
 
     public static final String QUEUE_NAME = "simple.queue";
 
-    @RabbitListener(queues = { QUEUE_NAME })
+    @RabbitListener(queues = {QUEUE_NAME})
     public void listenSimpleQueue(String message) {
         log.info("Spring consumer received: {}", message);
     }
