5653fede7e84276fc0331058c1135f78a1958458
ryanfawcett <tangch3ng@gmail.com>
2021-12-17 11:04:31 +0800

    feat: 采用Json消息转换器

---
+++ quickstart-rabbitmq/quickstart-mq-publisher/src/main/java/org/example/mq/config/MessageConvertConfig.java
@@ -0,0 +1,19 @@
+package org.example.mq.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConvertConfig {
+
+    /**
+     * 自定义Json消息转换器
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
